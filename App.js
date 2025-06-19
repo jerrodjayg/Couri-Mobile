938c8b55cb84ef5acc4922bd43f4b48566dbebce
ATC <Allthingscoding0@gmail.com>
2025-06-18 21:07:55 -0400

    fixed navigation from createaccount to welcome page

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from './screens/SplashScreen';
 import HomeScreen from './screens/HomeScreen';
 import CreateAccountScreen from './screens/CreateAccountScreen';
-import LoginScreen from './screens/LogInScreen'; //✅ Add this
+import LoginScreen from './screens/LogInScreen';
+import WelcomePage from './screens/Welcomepage'; // ✅ Add this line
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +17,8 @@ export default function App() {
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} /> {/* ✅ Add this line */}
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Welcomepage" component={WelcomePage} /> {/* ✅ Add this line */}
       </Stack.Navigator>
     </NavigationContainer>
   );
