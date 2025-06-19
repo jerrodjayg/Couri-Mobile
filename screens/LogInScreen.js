332f6083b742cfc32ee7b545489bf1bc4abd81b7
ATC <Allthingscoding0@gmail.com>
2025-06-18 20:22:00 -0400

    Fixed file path

--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -13,7 +13,7 @@ import {
   ScrollView,
 } from 'react-native';
 
-import { findUserByEmailOrPhone } from '../utils/UserStore';
+import { findUserByEmailOrPhone } from '../Couri-Mobile/UserStore.js';
 
 export default function LogInScreen({ navigation }) {
   const [emailOrPhone, setEmailOrPhone] = useState('');
