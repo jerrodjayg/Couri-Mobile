2eeaba6ee0673d80cb806ff380ed722dcd422509
JL Folly <jeanlucfolly@jls-macbook-air.myfiosgateway.com>
2025-07-02 00:09:35 -0400

    New Phone Auth

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -7,19 +8,27 @@ import HomeScreen from './screens/HomeScreen';
 import CreateAccountScreen from './screens/CreateAccountScreen';
 import LogInScreen from './screens/LogInScreen';
 import Welcomepage from './screens/Welcomepage'; // ✅ Ensure this matches the actual file and default export
+import CodeVerify from './screens/CodeVerify';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator();
 
+
 export default function App() {
   return (
+
+    <GestureHandlerRootView style={{ flex: 1 }}>
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
         <Stack.Screen name="Login" component={LogInScreen} />
+        <Stack.Screen name="CodeVerify" component={CodeVerify} />
         <Stack.Screen name="Welcomepage" component={Welcomepage} />
       </Stack.Navigator>
     </NavigationContainer>
+    </GestureHandlerRootView>
+  
   );
 }
